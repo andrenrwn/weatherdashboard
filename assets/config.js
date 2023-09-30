6beaa140b5453ca59806fa78276214d288bd71a8
anrwn <andrenrwn@users.noreply.github.com>
2023-09-30 12:26:09 +0800

    relocate config to make this publishable

---
+++ assets/config.js
@@ -0,0 +1,8 @@
+var myapikeys = {
+    unsplash: "hToCtEKI4Nj4xaC0jwg5Tva4DupP-vhbXhJ-mCiry5Y",
+    openweather: "2baf085ed1bbea0d1b7d521e3687a9b9",
+    openweatherfind: "439d4b804bc8187953eb36d2a8c26a02",
+    openweatheronecall: "9866fe0fd3c6de365412d2808c6f65d0",
+    google: "AIzaSyDAKGh9hM6lkhtz5MNmuUehgwnvtLVjYr8"
+};
+
